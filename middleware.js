da9c925c3c21f1812b3724927f17cd6cb93073ed
greatestman313-code <greatestman313@gmail.com>
2025-08-30 15:17:39 -0700

    Delete middleware.js

--- middleware.js
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from "next/server";
-export function middleware(req){ 
-  // Pass-through middleware to avoid runtime issues
-  return NextResponse.next(); 
-}
-export const config = { matcher: ["/((?!_next|.*\..*).*)"] };
