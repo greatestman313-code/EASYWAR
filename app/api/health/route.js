fb122dfcf03e47aae95a1dc104ca70d5c1489e0e
greatestman313-code <greatestman313@gmail.com>
2025-09-10 16:53:09 -0700

    Delete app directory

--- app/api/health/route.js
+++
@@ -1,8 +0,0 @@
-export async function GET() {
-  const ok = !!process.env.OPENAI_API_KEY;
-  return new Response(JSON.stringify({
-    ok,
-    hasOpenAIKey: ok,
-    version: "v1.2.1"
-  }), { status: ok ? 200 : 500, headers: { "content-type": "application/json" } });
-}
