69f2a8e0183c18ba3a4b198dcb41d970141c8fc8
greatestman313-code <greatestman313@gmail.com>
2025-09-12 05:16:51 -0700

    Delete app directory

--- app/help/center/page.jsx
+++
@@ -1,4 +0,0 @@
-export const dynamic = 'force-dynamic';
-export default function HelpCenterPage(){
-  return (<div className="p-6"><h1 className="text-xl font-bold mb-2">Help center</h1><p className="opacity-80">مقالات للمساعدة والتعلم.</p></div>);
-}
