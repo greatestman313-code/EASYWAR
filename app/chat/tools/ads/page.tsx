ba279c2d059820d588e580088f3deea4fc044de6
greatestman313-code <greatestman313@gmail.com>
2025-09-14 08:02:47 -0700

    Delete app directory

--- app/chat/tools/ads/page.tsx
+++
@@ -1,2 +0,0 @@
-export default function Page(){return <div className='p-6'>📊 صفحة تحليل الإعلانات (قابلة للتوسعة)
-</div>}
--- app/chat/tools/pricing/page.tsx
+++
@@ -1,2 +0,0 @@
-export default function Page(){return <div className='p-6'>💲 صفحة أداة التسعير (قابلة للتوسعة)
-</div>}
